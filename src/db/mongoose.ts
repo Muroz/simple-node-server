775e472a52bfadbd8824349952ccf50e865c544d
Muroz <dazc35@mun.ca>
2021-09-13 16:04:25 -0300

    - Added server dockerfile - Updated package scripts - Updated docker-compose file to include the server

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,8 +1,13 @@
 import mongoose from 'mongoose';
 
 const connect = async (): Promise<any> => {
-  const connection = await mongoose.connect('mongodb://localhost:27017');
-  return connection;
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URL);
+    return connection;
+  } catch (error) {
+    console.log('Connection failed. Please provide a valid MongoDB URL as the env variable named `MONGO_URL`');
+    return error;
+  }
 };
 
 export default connect;
